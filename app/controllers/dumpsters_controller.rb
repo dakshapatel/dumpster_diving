2fbd65cf93d91b603c2309d001093a428308ccc9
github username <github email address>
2018-09-18 16:44:57 -0700

    dumpster new form

--- app/controllers/dumpsters_controller.rb
+++ app/controllers/dumpsters_controller.rb
@@ -1,2 +1,14 @@
 class DumpstersController < ApplicationController
+
+    def index 
+        @dumpsters = Dumpster.all
+    end 
+
+    def new
+        @dumpster = Dumpster.new 
+    end 
+
+    def create 
+
+    end 
 end
