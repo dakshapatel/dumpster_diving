7e63d196ae2245b51e2a2f7b915a195f4a68ba6b
github username <github email address>
2018-11-15 18:31:58 -0700

    add edit, update, delete  methods to dumpster controller

--- app/controllers/dumpsters_controller.rb
+++ app/controllers/dumpsters_controller.rb
@@ -5,26 +5,48 @@ class DumpstersController < ApplicationController
         @dumpsters = Dumpster.all
     end 
 
+    
+
+    def show
+        @dumpster = Dumpster.find(params[:id])
+    end
+
     def new
         @dumpster = Dumpster.new
     end
-
-    def show
-        @dumpster = Dumpster.find_by(params[:id])
-    end
     
-    # def create 
-    #    @dumpster = Dumpster.new(dumpster_params)
-    #    if @dumpster.save
-    #     redirect_to dumpsters_path
-    #    else
-    #     render :new
-    #    end 
-    # end 
-
-    def edit
+    def create 
+        
+       @dumpster = Dumpster.new(dumpster_params)
+       if @dumpster.save
+        redirect_to dumpsters_path
+       else
+        render :new
+       end 
     end 
 
+    def edit
+        @dumpster = Dumpster.find(params[:id])
+    end 
+
+    def update
+        @dumpster = Dumpster.find(params[:id])
+        
+        @dumpster.update(dumpster_params)
+
+        if @dumpster.save
+            redirect_to dumpsters_path
+        else 
+            render :edit
+        end 
+    end 
+
+    def destroy 
+        @dumpster = Dumpster.find(params[:id])
+        @dumpster.destroy
+        flash[:notice] = "Dumpster Deleted."
+        redirect_to dumpsters_path
+    end 
 
     private
 
